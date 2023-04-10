fc76aa0bba85ac67f45976c42336650f995f87a5
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-04-10 17:37:45 -0300

    organizes the folders, creates a new activity and modifies the fragments

--- app/src/main/java/com/example/convidados/ui/PresentFragment.kt
+++ app/src/main/java/com/example/convidados/ui/PresentFragment.kt
@@ -1,4 +1,4 @@
-package com.example.convidados.ui.home
+package com.example.convidados.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,14 +7,14 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.convidados.databinding.FragmentHomeBinding
+import com.example.convidados.databinding.FragmentPresentBinding
 
-class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+class PresentFragment : Fragment() {
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private var _binding: FragmentPresentBinding? = null
+
+    // This property is only valid between onCreateView and onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -22,14 +22,14 @@ class HomeFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
+        val viewModel =
+            ViewModelProvider(this).get(PresentViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentPresentBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textGallery
+        viewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
