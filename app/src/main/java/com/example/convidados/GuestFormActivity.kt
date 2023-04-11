d21cead972b7fd941034c72895f69abc14373107
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-04-11 16:55:03 -0300

    starts the layout of activity that register a new guest and prepares the code to receive the data

--- app/src/main/java/com/example/convidados/GuestFormActivity.kt
+++ app/src/main/java/com/example/convidados/GuestFormActivity.kt
@@ -2,10 +2,32 @@ package com.example.convidados
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.ViewModelProvider
+import com.example.convidados.databinding.ActivityGuestFormBinding
 
-class GuestFormActivity : AppCompatActivity() {
+class GuestFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityGuestFormBinding
+    private lateinit var viewModel: GuestFormViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_guest_form)
+
+        //instancia o binding e infla o layout
+        binding = ActivityGuestFormBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //instancia a viewModel
+        viewModel = ViewModelProvider(this).get(GuestFormViewModel::class.java)
+
+        //deixa o status 'presente' marcado por padrão
+        binding.radioPresent.isChecked = true
+
+        binding.buttonSave.setOnClickListener(this)
+    }
+
+    //trata o botão save
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {}
     }
 }
