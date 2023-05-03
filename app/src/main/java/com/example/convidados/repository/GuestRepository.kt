8c91407292130c7b8d4f0f289d99f45c5d539500
NathaliaMiriam <nathalia.miriam@hotmail.com>
2023-05-03 19:01:10 -0300

    makes the data listing

--- app/src/main/java/com/example/convidados/repository/GuestRepository.kt
+++ app/src/main/java/com/example/convidados/repository/GuestRepository.kt
@@ -24,7 +24,7 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
-    //insere dado/convidado no banco de dados
+    //insere convidado no banco de dados
     fun insert(guest: GuestModel): Boolean {
         return try {
             val db = guestDataBase.writableDatabase
@@ -40,5 +40,92 @@ class GuestRepository private constructor(context: Context) {
         } catch (e: Exception) {
             false
         }
+    }
+
+    //atualiza o banco de dados onde(where) o id seja = a 'guest.id'
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase //abre a conexão com o banco
+            val presence = if (guest.presence) 1 else 0
+
+            //são os valores
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            //são os critérios
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + "= ?" //interpola com os valores do args ... "onde o ID for = a um valor"
+            val args = arrayOf(guest.id.toString()) //dentro passo os valores do tipo string ... passei 1, pois só tenho 1 ponto de interrogação e converti p string
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args) //tabela + vals e roda a aplicação ... acontece o update c os critérios alinhados
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    //deleta convidado do banco de dados pelo seu id
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase //abre a conexão com o banco
+
+            //são os critérios
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + "= ?" //interpola com os valores do args ... "onde o ID for = a um valor"
+            val args = arrayOf(id.toString()) //dentro passo os valores do tipo string ... passei 1, pois só tenho 1 ponto de interrogação e converti p string
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args) //tabela + vals e roda a aplicação ... acontece o delete c os critérios alinhados
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    //faz a seleção - listagem dos dados
+    fun getAll(): List<GuestModel> { //retorna uma lista de GuestModel
+
+        val list = mutableListOf<GuestModel>() //define o valor para que a lista seja retornada
+
+        try {
+            val db = guestDataBase.readableDatabase //abre a conexão com o banco e lê os dados
+
+            //array das colunas da tabela --> colocado ali na query()
+            val selection = arrayOf(
+                DataBaseConstants.GUEST.COLUMNS.ID,
+                DataBaseConstants.GUEST.COLUMNS.NAME,
+                DataBaseConstants.GUEST.COLUMNS.PRESENCE
+            )
+
+            //abre o cursor
+            val cursor = db.query(DataBaseConstants.GUEST.TABLE_NAME, selection, null, null, null, null, null)
+
+            //verificações
+            if (cursor != null && cursor.count > 0) {
+                while (cursor.moveToNext()) { //o cursor passa linha a linha da tabela
+
+                    //pega os valores ID - NAME - PRESENCE da tabela:
+
+                    //posição da coluna ID (index da col.) --> getInt() pois o ID é um inteiro
+                    val id = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.ID))
+
+                    //posição da coluna NAME (index da col.) --> getString() pois o NAME é um text
+                    val name = cursor.getString(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.NAME))
+
+                    //posição da coluna PRESENCE (index da col.) --> getInt() pois o PRESENCE retorna um inteiro
+                    val presence = cursor.getInt(cursor.getColumnIndex(DataBaseConstants.GUEST.COLUMNS.PRESENCE))
+
+
+                    //monta o objeto com os dados retornados - a cada passada de linha os dados são adicionados na lista
+                    list.add(GuestModel(id, name, presence == 1))
+                }
+            }
+
+            //fecha o cursor
+            cursor.close()
+
+        } catch (e: Exception) {
+            return list //retorna a lista vazia, caso caia no catch
+        }
+
+        return list //retorna a lista preenchida, caso não caia no catch
     }
 }
